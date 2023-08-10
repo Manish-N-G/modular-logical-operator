f19274862b18875714a4050e548ae7951aa595a7
Manish-N-G <rustieee@gmail.com>
2023-08-10 23:43:08 +0200

    2nd commit. This commit is after wasm-pack build was done. Created a pkg file

---
+++ pkg/logical_module_program.d.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function main_logic_function(): void;
---
+++ pkg/logical_module_program_bg.js
@@ -0,0 +1,11 @@
+let wasm;
+export function __wbg_set_wasm(val) {
+    wasm = val;
+}
+
+/**
+*/
+export function main_logic_function() {
+    wasm.main_logic_function();
+}
+
